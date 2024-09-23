5e3e2fc0defb7053a8fcfebe561811a3dc31c458
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2024-09-23 20:13:56 -0300

    refactor(stack): extract exception; fixed poms

---
+++ stack/src/main/java/stack/MaximumCapacity.java
@@ -0,0 +1,9 @@
+package stack;
+
+import java.io.Serial;
+
+class MaximumCapacity extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 5875002575184536190L;
+}
