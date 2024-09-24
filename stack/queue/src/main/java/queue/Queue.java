cbba3ba5b63bfdfc242f9299a9d8301ab573f0e1
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2024-09-23 21:16:36 -0300

    doc(queue): add doc for queue

--- stack/queue/src/main/java/queue/Queue.java
+++ stack/queue/src/main/java/queue/Queue.java
@@ -2,14 +2,33 @@ package queue;
 
 public interface Queue<E> {
 
+    /**
+     * Adds an element to the queue.
+     */
     void add(E e);
 
+    /**
+     * Returns whether the queue is empty.
+     */
     boolean isEmpty();
 
+    /**
+     * Gets the first element of the queue.
+     *
+     * <p>Throws {@link EmptyQueue} if the queue is empty.
+     */
     E peek();
 
+    /**
+     * Removes the first queue element.
+     *
+     * <p>Throws {@link EmptyQueue} if the queue is empty.
+     */
     E remove();
 
+    /**
+     * Returns the queue to its original state.
+     */
     void free();
 
     class EmptyQueue extends RuntimeException {}
