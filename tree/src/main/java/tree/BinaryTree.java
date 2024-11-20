6899f19a38074ecc9c1a4cc2b188d303e0819163
Lemuel Kauê Manske de Liz <lemuelkauemanskede.liz@philips.com>
2024-11-19 21:40:13 -0300

    feat: add remove

--- tree/src/main/java/tree/BinaryTree.java
+++ tree/src/main/java/tree/BinaryTree.java
@@ -4,10 +4,11 @@ public class BinaryTree<E extends Comparable<E>> {
 
     private Node<E> root;
 
-    public BinaryTree() { }
-
-    public BinaryTree(Node<E> root) {
-        this.root = root;
+    /**
+     * Adds an element to the tree.
+     */
+    public void add(E e) {
+        updateRoot(Node.of(e));
     }
 
     protected void updateRoot(Node<E> e) {
@@ -20,12 +21,12 @@ public class BinaryTree<E extends Comparable<E>> {
     private void updateRoot(Node<E> node, Node<E> e) {
         if (node.value().compareTo(e.value()) > 0)
             if (node.leftNode() == null)
-                node.setL(e);
+                node.setLeft(e);
             else
                 updateRoot(node.leftNode(), e);
         else
             if (node.rightNode() == null)
-                node.setR(e);
+                node.setRight(e);
             else
                 updateRoot(node.rightNode(), e);
     }
@@ -50,10 +51,6 @@ public class BinaryTree<E extends Comparable<E>> {
     public boolean has(E e) {
         if (isEmpty()) return false;
 
-        return has(root, e);
-    }
-
-    private boolean has(Node<E> node, E e) {
         Node<E> element = find(e);
 
         return element != null;
@@ -66,6 +63,12 @@ public class BinaryTree<E extends Comparable<E>> {
         return count(root);
     }
 
+    private int count(Node<E> root) {
+        if (root == null) return 0;
+
+        return 1 + count(root.leftNode()) + count(root.rightNode());
+    }
+
     /**
      * Returns whether the tree is degenerated.
      */
@@ -75,11 +78,13 @@ public class BinaryTree<E extends Comparable<E>> {
         return isDegenerated(root);
     }
 
-    /**
-     * Adds an element to the tree.
-     */
-    public void add(E e) {
-        updateRoot(Node.of(e));
+    private boolean isDegenerated(Node<E> node) {
+        if (node == null) return true;
+
+        if (node.leftNode() != null && node.rightNode() != null) return false;
+
+        return isDegenerated(node.leftNode())
+                && isDegenerated(node.rightNode());
     }
 
     /**
@@ -92,29 +97,16 @@ public class BinaryTree<E extends Comparable<E>> {
     }
 
     private Node<E> find(Node<E> node, E e) {
-        if (node == null) return null;
+        if (node == null)
+            return null;
 
-        if (node.value().equals(e)) return node;
+        if (node.value().equals(e))
+            return node;
 
         if (e.compareTo(node.value()) < 0)
             return find(node.left, e);
         else
             return find(node.right, e);
-    }
-
-    private boolean isDegenerated(Node<E> node) {
-        if (node == null) return true;
-
-        if (node.leftNode() != null && node.rightNode() != null) return false;
-
-        return isDegenerated(node.leftNode())
-                && isDegenerated(node.rightNode());
-    }
-
-    private int count(Node<E> root) {
-        if (root == null) return 0;
-
-        return 1 + count(root.leftNode()) + count(root.rightNode());
     }
 
     public String toString() {
@@ -134,12 +126,92 @@ public class BinaryTree<E extends Comparable<E>> {
      * Removes a node in the tree by matching its value.
      */
     public void remove(E e) {
+        Node<E> node = root;
+        Node<E> parent = null;
 
+        boolean leftChild = false;
+
+        while (node != null && node.value != e) {
+            parent = node;
+
+            if (e.compareTo(node.value) < 0) {
+                leftChild = true;
+                node = node.left;
+            } else {
+                leftChild = false;
+                node = node.right;
+            }
+        }
+
+        if (node == null)
+            return;
+
+        if (node.left == null && node.right == null) {
+            if (node == root) {
+                root = null;
+            } else {
+                if (leftChild) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
+        } else if (node.right == null) {
+            if (node == root) {
+                root = node.left;
+            } else {
+                if (leftChild) {
+                    parent.left = node.left;
+                } else {
+                    parent.right = node.left;
+                }
+            }
+        } else if (node.left == null) {
+            if (node == root) {
+                root = node.right;
+            } else {
+                if (leftChild) {
+                    parent.left = node.right;
+                } else {
+                    parent.right = node.right;
+                }
+            }
+        } else {
+            Node<E> successor = findSuccessor(node);
+
+            if (node == root) {
+                root = successor;
+            } else {
+                if (leftChild) {
+                    parent.left = successor;
+                } else {
+                    parent.right = successor;
+                }
+            }
+
+            successor.left = node.left;
+        }
     }
 
-    /**
-     * Represents a tree node.
-     */
+    private Node<E> findSuccessor(Node<E> node) {
+        Node<E> curr = node.right;
+        Node<E> successorParent = node;
+        Node<E> successor = node;
+
+        while (curr != null) {
+            successorParent = successor;
+            successor = curr;
+            curr = curr.left;
+        }
+
+        if (successor != node.right) {
+            successorParent.left = successor.right;
+            successor.right = node.right;
+        }
+
+        return successor;
+    }
+
     public static class Node<T> {
 
         private final T value;
@@ -193,11 +265,11 @@ public class BinaryTree<E extends Comparable<E>> {
             return value.toString();
         }
 
-        public void setL(Node<T> e) {
+        public void setLeft(Node<T> e) {
             left = e;
         }
 
-        public void setR(Node<T> e) {
+        public void setRight(Node<T> e) {
             right = e;
         }
     }
